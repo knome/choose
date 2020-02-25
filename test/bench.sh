5c0b596d4b22e24952c60bf5739aa4731dc513ab
Ryan Geary <rtgnj42@gmail.com>
2020-02-25 00:01:38 -0500

    Add test/bench.sh script
    bench.sh runs the `bench` command on each test/long*txt file with range
    3:5 and saves the output to a file for comparing performance across file
    sizes.

---
+++ test/bench.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+output="bench_output"
+mkdir -p $output
+inputs=($(find test -name "long*txt" | sort -r))
+for i in {0..4}
+do
+  echo ${inputs[$i]}
+  bench "target/release/choose 3:5 -i ${inputs[$i]}"  > $output/$1$i.bench
+done
